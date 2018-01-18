a49fd8f85b9bdc52e875858ae38619aef8ef87a6
jasonrwise77 <jasonrwise77@gmail.com>
2018-01-18 08:29:07 -0500

    Update battery-monitor.groovy

--- smartapps/jasonrwise77/battery-monitor.src/battery-monitor.groovy
+++ smartapps/jasonrwise77/battery-monitor.src/battery-monitor.groovy
@@ -1,3 +1,19 @@
+/**
+ *  Battery Monitor
+ *
+ *  Copyright 2017
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ */
 def smartAppNameFull() {
     return  "BatteryMonitor SmartApp for SmartThings"
 }
@@ -59,10 +75,10 @@ def smartAppLicense() {
 }
 
 definition(
-    name: "BatteryMonitor",
-    namespace: "notoriousbdg",
+    name: "Battery Monitor",
+    namespace: "jasonrwise77",
     author: "Brandon Gordon",
-    description: "SmartApp to monitor battery levels.",
+    description: "Battery Level Monitor.",
     category: "Convenience",
     iconUrl: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/smartapps/jasonrwise77/battery-monitor.src/icon.png",
     iconX2Url: "https://raw.githubusercontent.com/jasonrwise77/My-SmartThings/master/smartapps/jasonrwise77/battery-monitor.src/icon.png",
