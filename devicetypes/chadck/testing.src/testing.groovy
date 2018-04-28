2438e8264c4df7e6f323bd4e93b67ee08e6206f5
jasonrwise77 <jasonrwise77@gmail.com>
2018-04-27 21:37:30 -0400

    Update and rename devicetypes/chadck/z-wave-smart-fan-control.src/z-wave-smart-fan-control.groovy to devicetypes/chadck/testing.src/testing.groovy

--- devicetypes/chadck/testing.src/testing.groovy
+++ devicetypes/chadck/testing.src/testing.groovy
@@ -17,7 +17,7 @@
  *
  */
 metadata {
-	definition (name: "Z-Wave Smart Fan Control", namespace: "ChadCK", author: "ChadCK") {
+	definition (name: "testing", namespace: "chadck", author: "ChadCK") {
 		capability "Switch Level"
 		capability "Actuator"
 		capability "Indicator"
